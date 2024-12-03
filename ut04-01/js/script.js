d69056c8c688febdccbef74e891dcf150b792d0a
SamirElKharrat <71700525+SamirElKharrat@users.noreply.github.com>
2024-12-03 11:04:13 +0000

    Cambios
    Exacto, hay cambios

--- ut04-01/js/script.js
+++ ut04-01/js/script.js
@@ -1,27 +1,50 @@
 
-let titleCount = document.getElementById("titleCount");
-document.getElementById("title").addEventListener("keydown", (e) => {
-    titleCount.innerHTML = e.target.value.length + "/15";
+const DOM = {
+    titleCount : document.getElementById("titleCount"),
+    title : document.getElementById("title"),
+    area: document.getElementById("area"),
+    areaCount : document.getElementById("areaCount"),
+    dni: document.getElementById("dni"),
+    select : document.getElementById("stateSelect"),
+    frm: document.getElementById("frm"),
+}
+
+DOM.frm.addEventListener("submit", (e) => {
+    if(!DOM.title.validationMessage==""){
+        e.preventDefault();
+        alert(DOM.nomnre.validationMessage)
+    }
 })
 
-let areaCount = document.getElementById("areaCount");
-document.getElementById("area").addEventListener("keydown", (e) => {
-    areaCount.innerHTML = e.target.value.length + "/120";
+DOM.title.addEventListener("keydown", (e) => {
+    let currentLength = DOM.title.value.length + 1;
+    if (e.key === "Backspace") {
+        currentLength = currentLength - 2;
+    }
+    DOM.titleCount.textContent  = `${currentLength}/15`;
+
+})
+
+DOM.area.addEventListener("keydown", (e) => {
+    let currentLength = DOM.area.value.length + 1;
+    if (e.key === "Backspace") {
+        currentLength = currentLength - 2;
+    }
+    DOM.areaCount.textContent  = `${currentLength}/120`;
 })
 
 function ChangeState() {
-    let dni = document.getElementById("dni");
-    let value = document.getElementById("stateSelect").value;
+    let value = DOM.select.value;
     const patterns = {
         DNI: '[0-9]{8}[A-Z]',
         NIE: '[XYZ][0-9]{7}[A-Z]'
     };
 
     if (patterns[value]) {
-        dni.setAttribute('pattern', patterns[value]);
-        dni.removeAttribute('disabled');
+        DOM.dni.setAttribute('pattern', patterns[value]);
+        DOM.dni.removeAttribute('disabled');
     } else {
-        dni.setAttribute('disabled', 'disabled');
+        DOM.dni.setAttribute('disabled', 'disabled');
     }
 }
 
@@ -31,6 +54,5 @@ function passChange() {
     let isPassword = pass.type === 'password';
 
     pass.type = isPassword ? 'text' : 'password';
-    label.textContent = isPassword ? 'Ocultar' : 'Mostrar';
 }
 
